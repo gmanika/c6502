1484ba905b3c3cfd9c80104c02725c234f7c4a0c
Guilherme Manika <guilherme@zorked.net>
2014-05-16 14:04:14 -0300

    Fix ROM loading order

--- src-cljs/nes.cljs
+++ src-cljs/nes.cljs
@@ -4,18 +4,29 @@
             [goog.net.XhrIo]
             [goog.net.EventType :as eventtype]))
 
-(def nesROM (atom nil))
+(def NESConsole (atom nil))
+(def history (atom []))
+
+
+(add-watch NESConsole :history
+           (fn [_ _ _ n]
+             (when-not (= (last @history) n)
+               (swap! history conj n))))
+
+
+(defn undo
+  []
+  (swap! history pop)
+  (reset! NESConsole (pop @history)))
 
 (defn nes-loader
   [rom]
   (vec (concat (repeat 0xC000 0) (subvec rom 16))))
 
-
-
 (defn receiver [event]
   (let [response (.-response (.-target event))]
-    (compare-and-set! nesROM @nesROM (array-seq (js/Uint8Array. response)))))
-
+    (compare-and-set! NESConsole @NESConsole (c6502/CPU. 0 0 0 0xFD 0x36 0xC000 0
+                                                         (nes-loader (array-seq (js/Uint8Array. response)))))))
 
 (defn rom-loader
   []
@@ -26,39 +37,15 @@
     (.send req)))
 
 
+
 (rom-loader)
-@nesROM
-(subvec @nesROM 16)
-
-(def NESConsole (atom (c6502/CPU. 0 0 0 0xFD 0x36 0xC000 0 (nes-loader @nesROM))))
-
-
-(:pc @NESConsole)
-(count (:memory @NESConsole))
-
-(subvec (:memory @NESConsole) (:pc @NESConsole))
-
-(c6502/read-byte NESConsole 0xC00)
-
-(c6502/read-byte NESConsole 3072)
-
-(ui/render NESConsole)
-
-(js/console.log (dissoc @NESConsole :memory))
-(c6502/step @NESConsole)
-
 @NESConsole
+(c6502/step @NESConsole)
+(swap! NESConsole c6502/step @NESConsole)
+(undo)
+
+(count @history)
 
 (dissoc (swap! NESConsole c6502/step @NESConsole) :memory)
 
-(nth (:memory @NESConsole) 50991)
-
-
-(c6502/step @NESConsole)
-
-(c6502/step {:memory [0xE6 0 0 0 0]})
-
-(nth (:memory @NESConsole) (:pc @NESConsole))
-@nesROM
-(nth (:memory NESConsole) 0xC000)
 
